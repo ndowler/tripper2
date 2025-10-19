52e9d1af017d8f4f45ddfcd34a3a66adc6f0d434
xmerik <xmerik@proton.me>
2025-10-19 19:40:52 -0400

    Initial commit - dev branch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // or specify your allowed origins
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
